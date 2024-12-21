ec96678cb64c6e6ba224fdf06072fe07641c6701
NivedithaDharigond <nivedithadharigond@gmail.com>
2024-12-21 22:08:18 +0530

    Initial commit

--- AI_Study_Mate-main/static/game.js
+++ AI_Study_Mate-main/static/game.js
@@ -5,7 +5,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "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"; // Paste your API key here
+const API_KEY = ""; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
